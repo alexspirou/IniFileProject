d9457fc20ee98b6e398391712382eb494ec91636
Alex <alexspirou93@gmail.com>
2021-06-05 12:57:15 +0300

    validate

--- src/SpyrouProject/CIni.h
+++ src/SpyrouProject/CIni.h
@@ -1,6 +1,8 @@
 #pragma once
 #include <iostream>
 #include <fstream>
+#include <vector>
+#include <string>
 
 ////////////////////////////////////////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////////////////// 
@@ -20,12 +22,28 @@ public:
 	~CIni() {}
 
 
-	
-
-
-
 private:
 
+
+	void writeFile(std::string* userInput);
+
+	void readFile();
+
+	void validateFile();
+
+	std::ifstream inFile{};
+	std::ofstream outFile{};
+	std::string sFileName{ "Application.ini" };
+	std::vector<std::string> m_vSectionNames{ "[ -A SECTION- ]" , "[ -B SECTION- ]" };
+	size_t headerSize{ 0 };
+	std::string m_sNextLine{};
+
+	std::vector<std::string> m_vHeaders;
+	std::vector<std::string> vHeaderValues;
+	std::vector<std::string> m_vValData;
+
+	int indexSectionA{ 0 };
+	int indexSectionB{ 0 };
 
 };
 
