9d32bb03e1c698bc56e78838c6d90c729ffc46a9
Alex <alexspirou93@gmail.com>
2021-07-01 23:07:17 +0300

    validate ok

--- src/SpyrouProject/COptions.h
+++ src/SpyrouProject/COptions.h
@@ -11,15 +11,25 @@ public:
 	COptions();
 	~COptions() {}
 	
-
 	bool validate();
-
-	const std::string m_sIniFileName{ "Application.ini" };
-
 
 private:
 
-	std::vector<std::string> m_vDefaultValues{ 200 };
+	//Default values
+	std::string m_sVersion{ "2.7.8" };
+	std::string m_sLogFolder{ "--" };
+	std::string m_sLogFile{ "C:\\Log\LogFile.dbg" };
+	std::string m_sMaxThreads{ "1" };
+	std::string m_sMinCode{"-1"};
+	std::string m_sMaxCode{"1"};
+	std::string m_sResolution{ "100" };
+	std::string m_sRetrieveType{ "DISTANCE" };
+	std::string m_sCoverageMapAlgorithm{ "BASIC" };
+	std::string m_sIgnoreTxWithMissingLosses{"NO"};
+	std::string m_sMaxRadiusUnit{"KM"};
+	std::string m_sBasicParameters{" - 106.0,100,150.0,10000.0,1,9.0,1,-98.0,2.0,-98.0,3.0,1,0,0"};
+
+	const std::string m_sIniFileName{ "Application.ini" };
 
 
 };
--- src/SpyrouProject/main.cpp
+++ src/SpyrouProject/main.cpp
@@ -1,15 +1,20 @@
 #include "CIni.h"
 #include "COptions.h"
+
 int main() 
 {
 
 	COptions options;
 	CIni iniObj;
-	//Write file
 
+	iniObj.readFile();
 	iniObj.writeFile(0);
-	
 
+	if (!options.validate())
+	{
+		iniObj.writeFile(1);
+	}
+	if(!options.validate())
 
 	return 0;
 }
